1fa29b73ab965574034814e5bf5992edfa0e0113
oblacar <myoblacar@gmail.com>
2024-10-06 12:56:15 +0700

    Иконка с ховером собрана в отдельный компонент, ховер бар функционально готов

---
+++ src/components/IconHoverCardBar/IconHoverCard.jsx
@@ -0,0 +1,59 @@
+// src/components/IconHoverCard.js
+
+import React, { useState } from 'react';
+import styles from './IconHoverCardBar.module.css'; // Импортируем стили
+
+const IconHoverCard = ({
+    type,
+    iconRef,
+    IconComponent,
+    HoverCardComponent,
+    iconCoordinates,
+    windowWidth,
+}) => {
+    const [isHovered, setIsHovered] = useState(false); // Состояние для управления видимостью
+
+    const handleMouseEnter = () => {
+        setIsHovered(true); // Устанавливаем состояние в true при наведении
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false); // Устанавливаем состояние в false при уходе курсора
+    };
+
+    return (
+        <div
+            className={styles.iconHoverArea}
+            onMouseEnter={handleMouseEnter} // Обработчик для наведения
+            onMouseLeave={handleMouseLeave} // Обработчик для ухода мыши
+        >
+            <div
+                className={styles.headerMarketIcon}
+                ref={iconRef}
+            >
+                <div className={styles.iconContainer}>
+                    <IconComponent
+                        className={`${styles.iconHover} ${
+                            isHovered ? styles.iconHovered : ''
+                        }`}
+                    />
+                    <span
+                        className={`${styles.iconLabel} ${
+                            isHovered ? styles.iconHovered : ''
+                        }`}
+                    >
+                        {type}{' '}
+                        {/* Здесь можно заменить на текст, если необходимо */}
+                    </span>
+                </div>
+            </div>
+            <HoverCardComponent
+                isHoveredIcon={isHovered}
+                iconCoordinates={iconCoordinates}
+                windowWidth={windowWidth}
+            />
+        </div>
+    );
+};
+
+export default IconHoverCard;
---
+++ src/components/IconHoverCardBar/IconHoverCardBar.jsx
@@ -0,0 +1,120 @@
+import { Link } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
+
+import IconHoverCard from './IconHoverCard';
+
+import { HoverUserCard } from './hoverCard/HoverUserCard';
+import { HoverCartCard } from './hoverCard/HoverCartCard';
+import { HoverMessageCard } from './hoverCard/HoverMessageCard';
+import { HoverOrdersCard } from './hoverCard/HoverOrdersCard';
+import styles from './IconHoverCardBar.module.css';
+
+//импорт иконок из Font Awesome коллекции в react-icons. При необходимости можно выбрать другие коллекции и иконки
+import {
+    FaUser,
+    FaEnvelope,
+    FaShippingFast,
+    FaClipboardList,
+    FaShoppingCart,
+} from 'react-icons/fa';
+
+function setCoordinates(iconUserRef, setIconCoordinates) {
+    const iconElement = iconUserRef.current;
+    if (iconElement) {
+        const rect = iconElement.getBoundingClientRect();
+
+        setIconCoordinates(() => {
+            return {
+                top: rect.top,
+                left: rect.left,
+                bottom: rect.bottom,
+                right: rect.right,
+            };
+        });
+    }
+}
+
+const zero = {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+};
+
+function IconDropdownMenuBar() {
+    const [iconUserCoordinates, setIconUserCoordinates] = useState(zero);
+    const [iconOrdersCoordinates, setIconOrdersCoordinates] = useState(zero);
+    const [iconMessageCoordinates, setIconMessageCoordinates] = useState(zero);
+    const [iconCartCoordinates, setIconCartCoordinates] = useState(zero);
+
+    const iconUserRef = useRef(null);
+    const iconOrdersRef = useRef(null);
+    const iconMessageRef = useRef(null);
+    const iconCartRef = useRef(null);
+
+    const [windowWidth, setWindowWidth] = useState(
+        document.documentElement.clientWidth
+    );
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(document.documentElement.clientWidth);
+
+            setCoordinates(iconUserRef, setIconUserCoordinates);
+            setCoordinates(iconOrdersRef, setIconOrdersCoordinates);
+            setCoordinates(iconMessageRef, setIconMessageCoordinates);
+            setCoordinates(iconCartRef, setIconCartCoordinates);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <>
+            <div className={styles.iconsArea}>
+                <IconHoverCard
+                    type='Профиль'
+                    iconRef={iconUserRef}
+                    IconComponent={FaUser}
+                    HoverCardComponent={HoverUserCard}
+                    iconCoordinates={iconUserCoordinates}
+                    windowWidth={windowWidth}
+                />
+                <IconHoverCard
+                    type='Сообщения'
+                    iconRef={iconMessageRef}
+                    IconComponent={FaEnvelope}
+                    HoverCardComponent={HoverMessageCard}
+                    iconCoordinates={iconMessageCoordinates}
+                    windowWidth={windowWidth}
+                />
+
+                <IconHoverCard
+                    type='Заказы'
+                    iconRef={iconOrdersRef}
+                    IconComponent={FaClipboardList}
+                    HoverCardComponent={HoverOrdersCard}
+                    iconCoordinates={iconOrdersCoordinates}
+                    windowWidth={windowWidth}
+                />
+
+                <IconHoverCard
+                    type='Корзина'
+                    iconRef={iconCartRef}
+                    IconComponent={FaShoppingCart}
+                    HoverCardComponent={HoverCartCard}
+                    iconCoordinates={iconCartCoordinates}
+                    windowWidth={windowWidth}
+                />
+            </div>
+        </>
+    );
+}
+
+export { IconDropdownMenuBar };
