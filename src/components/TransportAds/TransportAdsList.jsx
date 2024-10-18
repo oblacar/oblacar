6e9bf7f4e6c6d68f4919bbac04f21eb7c832e9d3
oblacar <myoblacar@gmail.com>
2024-10-18 16:06:45 +0700

    Сделал компонент прелоадер, добавиго его к объявлениям

--- src/components/TransportAds/TransportAdsList.jsx
+++ src/components/TransportAds/TransportAdsList.jsx
@@ -3,15 +3,21 @@ import TransportAdContext from '../../hooks/TransportAdContext';
 import TransportAdItem from './TransportAdItem';
 import styles from './TransportAdsList.module.css';
 
+import Preloader from '../common/Preloader/Preloader';
+
 const TransportAdsList = () => {
     const { ads, loading, error } = useContext(TransportAdContext);
 
-    if (loading) return <div className={styles.loadingMessage}>Loading...</div>;
+    if (loading) return (
+        <div className={styles.preloader}>
+            <Preloader />
+        </div>
+    );
     if (error) return <div className={styles.errorMessage}>Error: {error}</div>;
 
-    const getRandomRating = () => {
-        return Math.floor(Math.random() * 50) / 10;
-    };
+    // const getRandomRating = () => {
+    //     return Math.floor(Math.random() * 50) / 10;
+    // };
 
     return (
         <div className={styles.adsListContainer}>
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Preloader.css'; // Убедитесь, что вы импортируете CSS-файл
+
+const Preloader = () => {
+    return (
+        <div className='loader'></div> // Ваш прелоадер
+    );
+};
+
+export default Preloader;
