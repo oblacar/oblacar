0da2c3fd95e243e8327f8b833b38d3acbc8503ec
oblacar <myoblacar@gmail.com>
2024-10-30 17:41:50 +0700

    Подсказка над добавлением в Варианты

--- src/components/common/ToggleIconButtonPlus/ToggleIconButtonPlus.jsx
+++ src/components/common/ToggleIconButtonPlus/ToggleIconButtonPlus.jsx
@@ -1,24 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import './ToggleIconButtonPlus.css';
 
 const ToggleIconButtonPlus = ({ onToggle, initialAdded = false }) => {
     const [isAdded, setIsAdded] = useState(initialAdded);
 
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    // Обработка задержки при наведении
+    const handleMouseEnter = () => {
+        setTimeout(() => {
+            setShowTooltip(true);
+        }, 500); // Задержка в 0.5 секунды
+    };
+
+    const handleMouseLeave = () => {
+        setShowTooltip(false);
+    };
+
     const handleClick = () => {
         setIsAdded((prev) => !prev);
         onToggle(!isAdded);
     };
 
+    useEffect(() => {
+        setIsAdded(initialAdded);
+    }, [initialAdded]);
+
     return (
         <button
             onClick={handleClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
             className={`toggle-icon-button ${isAdded ? 'added' : 'not-added'}`}
         >
-            <FaPlus className='icon' />
-            {/* <span className='toggle-label'>
-                {isAdded ? 'Удалить из вариантов' : 'Добавить в варианты'}
-            </span> */}
+            <div className='toggle-label-container-tip'>
+                <FaPlus className='icon' />
+                {showTooltip && (
+                    <span className='toggle-label-tip'>
+                        {isAdded
+                            ? 'Удалить из Вариантов'
+                            : 'Добавить в Варианты'}
+                    </span>
+                )}
+            </div>
         </button>
     );
 };
