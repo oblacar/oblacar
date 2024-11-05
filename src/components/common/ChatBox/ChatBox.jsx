4863dbe2d185249eda6bc84f2d537806adc5d8ba
oblacar <myoblacar@gmail.com>
2024-11-05 16:00:57 +0700

    Мышкой меняем высоту чата

--- src/components/common/ChatBox/ChatBox.jsx
+++ src/components/common/ChatBox/ChatBox.jsx
@@ -1,5 +1,5 @@
 // src/components/ChatBox.js
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useRef, useState } from 'react';
 import ConversationContext from '../../../hooks/ConversationContext';
 
 import MessagesList from './MessagesList/MessagesList';
@@ -9,10 +9,35 @@ import './ChatBox.css';
 import AuthContext from '../../../hooks/Authorization/AuthContext';
 
 const ChatBox = ({ onClose }) => {
+    const { userId } = useContext(AuthContext);
     const { selectedConversation, messages, sendMessage } =
         useContext(ConversationContext);
-    const { userId } = useContext(AuthContext);
 
+    const [height, setHeight] = useState(400); // Начальная высота чата
+    const chatBoxRef = useRef(null);
+    const startYRef = useRef(0); // Используем useRef для стартовой позиции
+
+    /// Растягивание области чата мышкой--->>>
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        startYRef.current = e.clientY; // Сохраняем начальную позицию мыши в startYRef
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+    };
+
+    const handleMouseMove = (e) => {
+        const deltaY = startYRef.current - e.clientY; // Смещение мыши вверх/вниз
+        setHeight((prevHeight) => Math.max(200, prevHeight + deltaY));
+        startYRef.current = e.clientY; // Обновляем позицию мыши для плавного изменения
+    };
+
+    const handleMouseUp = () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    };
+    //<<<---
+
+    //Отправка сообщений
     const handleSendMessage = (text) => {
         if (selectedConversation) {
             sendMessage(selectedConversation.conversationId, userId, text);
@@ -27,7 +52,15 @@ const ChatBox = ({ onClose }) => {
 
     return (
         selectedConversation && (
-            <div className='chatbox'>
+            <div
+                className='chatbox'
+                ref={chatBoxRef}
+                style={{ height: `${height}px` }}
+            >
+                <div
+                    className='resize-handle'
+                    onMouseDown={handleMouseDown}
+                />
                 <div className='chatbox-header'>
                     <span>Переписка</span>
                     <button
