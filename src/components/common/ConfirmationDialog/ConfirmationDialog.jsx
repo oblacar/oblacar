4e8f0d1f7db40c3ce92fe8c7e3c86b349dd9527e
oblacar <myoblacar@gmail.com>
2024-11-11 22:08:33 +0700

    Новое объявление. Ситилизация Конфермации.

--- src/components/common/ConfirmationDialog/ConfirmationDialog.jsx
+++ src/components/common/ConfirmationDialog/ConfirmationDialog.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Button from '../Button/Button';
+import './ConfirmationDialog.css';
 
 const ConfirmationDialog = ({ message, onConfirm, onCancel }) => (
     <div className='confirmation-dialog'>
         <p>{message}</p>
-        <Button
-            onClick={onConfirm}
-            type_btn='yes'
-        >
-            Да
-        </Button>
-        <Button
-            onClick={onCancel}
-            type_btn='no'
-        >
-            Отмена
-        </Button>
+        <div className='confirmation-dialog-buttons'>
+            <Button
+                onClick={onConfirm}
+                type_btn='yes'
+            >
+                Да
+            </Button>
+            <Button
+                onClick={onCancel}
+                type_btn='no'
+            >
+                Отмена
+            </Button>
+        </div>
     </div>
 );
 
