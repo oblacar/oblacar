0feb661f3c9681ddb2bb5923e3e8676f414e389c
oblacar <myoblacar@gmail.com>
2024-11-17 17:27:38 +0700

    Траспортные запросы. Стилизация

--- src/components/AdProfile/RequestStatusBlock.jsx
+++ src/components/AdProfile/RequestStatusBlock.jsx
@@ -3,6 +3,9 @@ import Button from '../common/Button/Button';
 
 import './RequestStatusBlock.css';
 
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/outline';
+// check - circle;
+
 const RequestStatusBlock = ({
     status,
     onCancelRequest,
@@ -14,13 +17,29 @@ const RequestStatusBlock = ({
             <strong>Статус вашего запроса:</strong>
             <p>"{adTransportationRequest.requestData.description}"</p>
             <p className='transport-ad-profile-request-status-info'>
-                {status === 'pending' &&
-                    'Запрос отправлен, ждем ответа владельца.'}
+                {status === 'pending' && (
+                    <div className='request-status-container pending'>
+                        <CheckCircleIcon className='request-status-icon' />
+                        <span>Запрос отправлен, ждем ответа владельца.</span>
+                    </div>
+                )}
+
                 {status === 'accepted' &&
                     'Ваш запрос принят! Свяжитесь с владельцем для деталей.'}
                 {status === 'declined' && 'Ваш запрос отклонен владельцем.'}
+                {/* 
                 {status === 'cancelled' &&
-                    'Вы отменили запрос! Но вы можете отправить новый запрос.'}
+                    'Вы отменили запрос! Но вы можете отправить новый запрос.'} */}
+                {status === 'cancelled' && (
+                    <div className='request-status-container cancelled'>
+                        <XCircleIcon className='request-status-icon' />
+                        <span>
+                            Вы отменили запрос! Но вы можете отправить новый
+                            запрос.
+                        </span>
+                    </div>
+                )}
+
                 {status === 'inProgress' && 'Перевозка в процессе.'}
                 {status === 'completed' && 'Перевозка завершена.'}
                 {status === 'failed' && 'Перевозка завершена с ошибкой.'}
@@ -29,7 +48,7 @@ const RequestStatusBlock = ({
             {status === 'pending' && (
                 <Button
                     type='button'
-                    type_btn='no'
+                    type_btn='reverse-no'
                     children='Отменить запрос'
                     onClick={onCancelRequest}
                 />
@@ -38,7 +57,7 @@ const RequestStatusBlock = ({
             {status === 'cancelled' && (
                 <Button
                     type='button'
-                    children='Сделать новый запрос'
+                    children='Новый запрос'
                     onClick={onRestartRequest}
                 />
             )}
