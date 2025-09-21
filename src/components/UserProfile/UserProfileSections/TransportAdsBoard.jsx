37879fe6d69d1dfb4e06853841f4dc9309b1e955
asus (Oblacar) <myoblacar@gmail.com>
2025-09-21 14:25:24 +0700

    доска объявлений транспорта добавлен. Фиксим баги

--- src/components/UserProfile/UserProfileSections/TransportAdsBoard.jsx
+++ src/components/UserProfile/UserProfileSections/TransportAdsBoard.jsx
@@ -2,41 +2,42 @@
 import React, { useContext } from 'react';
 import ProfileSectionCard from '../../common/ProfileSectionCard/ProfileSectionCard';
 import InlineOverlapRow from '../../common/ProfileSectionCard/InlineOverlapRow';
-import VehicleAvatar from '../../VehicleCard/VehicleAvatar';
-import { VehicleContext } from '../../../hooks/VehicleContext';
+import TransportAdMiniCard from '../../TransportAds/TransportAdMiniCard/TransportAdMiniCard'
+
+import TransportAdContext from '../../../hooks/TransportAdContext';
 
 const TransportAdsBoard = () => {
-    const { vehicles = [], loading, error } = useContext(VehicleContext);
+  const { ads = [], loading, error } = useContext(TransportAdContext);
 
-    return (
-        <ProfileSectionCard
-            className="profile-section--fill profile-section--stick-bottom"
-            title="Мои объявления о Траспорте"
-            subtitle="Здесь будут размещенные вами объявления о наличие Транспорта для перевозки."
-            toList="/vehicles"
-            items={vehicles}
-            emptyText="Пока нет объявлений"
-            renderContent={() => {
-                if (loading) return <div style={{ padding: '6px 0' }}>Загрузка…</div>;
-                if (error) return <div className="error-text">Ошибка: {String(error)}</div>;
-                if (!vehicles.length) return <div className="profile-section-card__empty-text">Пока нет машин</div>;
+  return (
+    <ProfileSectionCard
+      className="profile-section--fill profile-section--stick-bottom"
+      title="Мои объявления о Траспорте"
+      subtitle="Здесь будут размещенные вами объявления о наличие Транспорта для перевозки."
+      toList="/vehicles"
+      items={ads}
+      emptyText="Пока нет объявлений"
+      renderContent={() => {
+        if (loading) return <div style={{ padding: '6px 0' }}>Загрузка…</div>;
+        if (error) return <div className="error-text">Ошибка: {String(error)}</div>;
+        if (!ads.length) return <div className="profile-section-card__empty-text">Пока нет машин</div>;
 
-                return (
-                    <InlineOverlapRow
-                        items={vehicles}
-                        ItemComponent={({ item, size }) => (
-                            <VehicleAvatar vehicle={item} size={size} />
-                        )}
-                        itemSize={60}
-                        itemAspectRatio={1}
-                        gap={8}
-                        getKey={(v) => v.truckId}
-                        buildTo={(v) => `/vehicles/${v.truckId}`}
-                    />
-                );
-            }}
-        />
-    );
+        return (
+          <InlineOverlapRow
+            items={ads}
+            ItemComponent={({ item, size }) => (
+              <TransportAdMiniCard ad={item} width={size} />
+            )}
+            itemSize={60}
+            itemAspectRatio={1}
+            gap={8}
+            getKey={(v) => v.adId}
+            buildTo={(v) => `/vehicles/${v.adId}`}
+          />
+        );
+      }}
+    />
+  );
 };
 
 export default TransportAdsBoard;
