fd049b8af7fb018af9ede532ddc7d33a4b9fcca8
asus (Oblacar) <myoblacar@gmail.com>
2025-10-06 13:22:22 +0700

    Пиллинг в Транспорт эд

--- src/components/AdProfile/OtherTransportAdDetails.jsx
+++ src/components/AdProfile/OtherTransportAdDetails.jsx
@@ -24,14 +24,47 @@ const OtherTransportAdDetails = ({ ad }) => {
     } = data || {};
 
     const loadingTypesItem = () => {
-        const s = Array.isArray(loadingTypes) ? loadingTypes.join(', ') : '';
-        return s ? <>{s}</> : '—';
+        // const s = Array.isArray(loadingTypes) ? loadingTypes.join(', ') : '';
+        // return s ? <>{s}</> : '—';
+
+        return Array.isArray(loadingTypes) && loadingTypes.length ? (
+            <div className='oatp-tag-list'>
+                {loadingTypes.map((label, index) => (
+                    <span key={index} className='oatp-tag'>
+                        {label}
+                    </span>
+                ))}
+            </div>
+        ) : (
+            loadingTypes || ''
+        )
     };
 
     const paymentOptionsItem = () => {
-        let s = Array.isArray(paymentOptions) ? paymentOptions.join(', ') : '';
-        if (readyToNegotiate) s = s ? `${s}, торг` : 'торг';
-        return s || '—';
+        const list = Array.isArray(paymentOptions) ? paymentOptions : [];
+
+        if (list.length || readyToNegotiate) {
+            return (
+                <div className='oatp-tag-list'>
+                    {/* Рендеринг основных тегов */}
+                    {list.map((label, index) => (
+                        <span key={index} className='oatp-tag'>
+                            {label}
+                        </span>
+                    ))}
+
+                    {/* Условный рендеринг тега "торг" с использованием логического И (&&) */}
+                    {readyToNegotiate && (
+                        <span key="negotiate" className='oatp-tag'>
+                            торг
+                        </span>
+                    )}
+                </div>
+            );
+        }
+
+        // Если ничего нет
+        return '';
     };
 
     const truckWeightValue = () => {
@@ -65,8 +98,11 @@ const OtherTransportAdDetails = ({ ad }) => {
                     <strong>Цена: </strong>{price ? `${formatNumber(String(price))} ${paymentUnit || ''}` : '—'}
                 </div>
                 <div className="other-ad-profile-rout-date-price-row">
-                    <strong>Условия: </strong>{paymentOptionsItem()}
+                    {/* <strong>Условия: </strong> */}
+                    {paymentOptionsItem()}
                 </div>
+
+                <div className='other-ad-profile-separator' />
 
                 <div className="other-ad-profile-truck">
                     <div className="other-ad-profile-truck-row">
